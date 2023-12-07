6707020016e184a03ee800976e1772858a07df77
Rajesh000730 <rajeshrachit09@gmail.com>
2023-12-07 14:18:47 +0530

    download m4a file

---
+++ scripts/download-audio.sh
@@ -0,0 +1,6 @@
+
+VIDEO_ID=$1
+
+[ -z "$VIDEO_ID" ] && echo "ERROR: no video ID specified" && exit 1
+
+yt-dlp "https://www.youtube.com/watch?v=$VIDEO_ID" --format m4a -o "./tmp/%(id)s.%(ext)s" 2>&1
